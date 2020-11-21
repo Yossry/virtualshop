74970bd09f18cf4c57d66075252bf3677c41b0ec
shaker66 <mohamed.shakerr90@gmail.com>
2020-11-21 16:34:49 +0200

    pragmatic_quickbooks_connector

---
+++ pragmatic_quickbooks_connector/models/qbo_logger.py
@@ -0,0 +1,10 @@
+from odoo import api, fields, models, _
+
+class QBOLoger(models.Model):
+    _name = 'qbo.logger'
+    _rec_name = 'odoo_name'
+
+    odoo_name = fields.Char('Name')
+    odoo_object = fields.Char('Object')
+    message = fields.Char('Message')
+    created_date = fields.Datetime('Created Date')
