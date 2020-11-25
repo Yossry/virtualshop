41765d79e671492e8233852999ec8c7ea2226e93
shaker66 <mohamed.shakerr90@gmail.com>
2020-11-25 19:03:05 +0200

    sh_product_discount

---
+++ sh_product_discount/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name" : "Product Discount Limitation",
+    "author" : "Softhealer Technologies",
+    "website": "https://www.softhealer.com",
+    "support": "support@softhealer.com",        
+    "category": "Extra Tools",
+    "summary": """
+                
+                    """,
+    "description": """
+        
+                    """,    
+    "version":"13.0.1",
+    "depends" : ["base","sale_management"],
+    "application" : True,
+    "data" : [
+        'views/sh_product_view.xml',
+            ],            
+    "images": ["static/description/background.png",],
+    "live_test_url": "https://youtu.be/XSawVjknUtI",              
+    "auto_install":False,
+    "installable" : True,  
+	"price": "25",
+	"currency": "EUR"    
+}
